4fc000907f350fd95d26073f1da3cafcc32d3019
Anthony Clint Pastorello <dev.anthonyclint@gmail.com>
2022-12-30 19:18:42 -0300

    feat: Banner da aplicação.

---
+++ organo/src/components/banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css';
+
+function Banner(){
+    return (
+        <header className="banner">
+            <img src="/img/banner.png" alt="banner do organograma." />
+        </header>
+    )
+}
+
+export default Banner
